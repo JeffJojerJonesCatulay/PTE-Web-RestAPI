98904e8a43e7a791ccaa13716bdafa00c831aba1
Jeff Catulay <catulay.programming.acc@gmail.com>
2023-09-23 09:41:14 +0800

    initial commit

---
+++ src/main/java/PTE/WEB_RESTAPI/WebRestapiApplication.java
@@ -0,0 +1,13 @@
+package PTE.WEB_RESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRestapiApplication.class, args);
+	}
+
+}
